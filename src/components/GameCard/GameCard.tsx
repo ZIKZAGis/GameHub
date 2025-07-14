41c6aaec2e2a8c20b1d34af6ef7a394195e52e86
Evgeniy <nikolaev_zheka-zik@bk.ru>
2025-07-14 14:39:16 +0300

    add coming soon games

--- src/components/GameCard/GameCard.tsx
+++ src/components/GameCard/GameCard.tsx
@@ -4,11 +4,11 @@ import { IPlatform, NamedEntity } from '@/types/game'
 
 type GameCardProps = {
     name: string, 
-    released: string, 
-    platforms: IPlatform[], 
+    released?: string, 
+    platforms?: IPlatform[], 
     background_image: string, 
-    rating: number, 
-    genres: NamedEntity[],
+    rating?: number, 
+    genres?: NamedEntity[],
     onClick: () => void
 }
 
@@ -36,10 +36,10 @@ export default function GameCard({
         <div className='absolute bottom-0 left-0 p-4 w-full'>
             <h3 className='text-white font-bold text-xl truncate'>{name}</h3>
             <div className='grid grid-cols-2 gap-1 items-center text-xs text-gray-300 mt-1'>
-                <span>{new Date(released).toLocaleDateString()}</span>
-                <span>{platforms.map((pl) => pl.platform.name).join(" | ")}</span>
-                <span>Rating: {rating}</span>
-                <span>{genres[0].name}</span>
+                {released && <span>Release date: {new Date(released).toLocaleDateString()}</span>}
+                {platforms && <span>{platforms.map((pl) => pl.platform.name).join(" | ")}</span>}
+                {rating != undefined && <span>{rating > 0 ? `Rating: ${rating}` : 'No rating'}</span>}
+                {genres && <span>{genres[0].name}</span>}
             </div>
         </div>
     </div>
