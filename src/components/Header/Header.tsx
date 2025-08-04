41cfbb6c9fe108f12464d5218961d8ec24470295
Евгений <86608474+ZIKZAGis@users.noreply.github.com>
2025-08-04 14:51:30 +0300

    Features/game list (#21)
    * feat: implemented genre filtering and optimized url param construction in usegamelist and api route
    
    * add gamefilterpanel component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,18 @@
+"use client"
+
 import Link from "next/link";
 import GameSearchInput from "../GameSearchInput/GameSearchInput";
+import { usePathname } from "next/navigation";
 
 export default function Header() {
+  const pathname = usePathname()
+
   return (
     <div className="w-full">
       <div className="flex justify-between w-full max-w-7xl items-center justify-items-center m-auto p-4 border-b-2 border-b-[#ff5338]">
         <Link href="/">Logo</Link>
 
-        <GameSearchInput />
+        {pathname !== '/games_list' && <GameSearchInput />}
 
         <Link href="/games_list">Game list</Link>
         <Link href="/auth_page">Icon</Link>
